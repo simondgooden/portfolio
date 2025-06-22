085fb001fad58632b2dd20a8a5eb286b04472ba8
simondgooden <sdgooden@ucdavis.edu>
2025-06-21 18:59:35 -0700

    added dark and light toggle and star background to dark mode

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,12 @@
+import { StarBackground } from "@/components/StarBackground";
+import { ThemeToggle } from "@/components/ThemeToggle";
+
 export const Home = () => {
     return(
-        <div className="min-h-screen ">
-            
-            
+        <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+            <ThemeToggle />   
+
+            <StarBackground />         
         </div>
     );
 };
