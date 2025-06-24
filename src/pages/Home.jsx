2481f89ac3627f8e7a5d9b83379e7944b4ec50a5
simondgooden <sdgooden@ucdavis.edu>
2025-06-23 17:10:52 -0700

    navbar and responsive mobile menu addded

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,7 +9,9 @@ export const Home = () => {
 
             <StarBackground />
 
-            <Navbar />         
+            <Navbar />   
+            
+                  
         </div>
     );
 };
